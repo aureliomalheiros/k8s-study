27f00fe045f711fabc7023f4f88ce75b4342e4d1
Aurelio Malheiros <aureliomalheiros@gmail.com>
2023-12-05 14:50:06 -0300

    remove .lock.hcl and modify file k8s.tf

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,8 +1,10 @@
 module "k8s" {
-  source            = "../../aws-modules-terraform/k8s" // replace with the path to your module
-  count             = 2
-  key_name          = "k8s.aureliomalheiros.tech"
-  availability_zone = "us-east-1a"
-  instance_type     = "t3a.small"
-  tags_instance     = { "Name" = "k8s-instance-${count.index + 1}.aureliomalheiros.tech" }
+  source                 = "git@github.com:aureliomalheiros/aws-modules-terraform.git//k8s" // replace with the path to your module
+  worker_count           = 1
+  key_name               = "k8s.aureliomalheiros.tech"
+  availability_zone      = "us-east-1a"
+  instance_type          = "t3a.small"
+  tags_instance          = "k8s-instance-workers"
+  subnet_id              = "subnet-03bda73c3706511b9"
+  vpc_security_group_ids = ["sg-030b02d63f0588700"]
 }
