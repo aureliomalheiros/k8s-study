c368e3434a0f6f45cc75888bf4d8012a25cc5e6f
Aurelio Malheiros <aureliomalheiros@gmail.com>
2024-11-15 14:48:36 -0300

    delete resources

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default = {
-    Name        = "k8s-vpc"
-    Component   = "k8s"
-    Environment = "study"
-  }
-}
